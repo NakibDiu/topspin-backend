edb45b35a77f949a3e966d941f9752feb38d6c95
NakibDiu <nakib15-3358@diu.edu.bd>
2023-06-14 00:44:26 +0600

    save user data to the database

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Top is spinning");
 });
 
-const dbUser = process.env.DB_USER
-const dbPass = process.env.DB_PASS
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
 
 const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.g4f1thy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,32 +23,47 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
-    const classCollection = client.db("campDB").collection("classes"); 
+    const classCollection = client.db("campDB").collection("classes");
+    const usersCollection = client.db("campDB").collection("users");
 
-
+    //classes api
     app.get("/classes", async (req, res) => {
-        const cursor = await classCollection.find().toArray();
-        res.send(cursor);
-    })
+      const cursor = await classCollection.find().toArray();
+      res.send(cursor);
+    });
 
+    // users api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log(`Top spin is listening on port ${port}`);
